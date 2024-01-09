1a256b318b2e67b73108f14a40d52b867d7017ae
user595 <dwightwilkins@gmail.com>
2024-01-08 20:13:51 -0600

    Allow command line parameters

--- GenerateChecksum.py
+++ GenerateChecksum.py
@@ -1,14 +1,22 @@
+"""
+generate a MD5 checksum of the filename specified
+i.e. /bin/python3 ~/Programming/Python/GenerateChecksum/GenerateChecksum.py GenerateChecksum.py 
+MD5 checksum for GenerateChecksum.py: beec6281d78aad4a3c18d97dfad80518
+"""
 import hashlib
+import sys
 
 def generate_checksum(file_path):
     md5 = hashlib.md5()
     with open(file_path, 'rb') as file:
-        # Read and update hash string value in blocks of 4K
         for byte_block in iter(lambda: file.read(4096), b""):
             md5.update(byte_block)
     return md5.hexdigest()
 
-# Example usage:
-file_path = 'your_file_path_here.txt'
+if len(sys.argv) != 2:
+    print("Usage: python checksum_generator.py <file_path>     to generate a MD5 checksum")
+    sys.exit(1)
+
+file_path = sys.argv[1]
 checksum = generate_checksum(file_path)
 print(f"MD5 checksum for {file_path}: {checksum}")
