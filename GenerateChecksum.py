54614faaf651d3cb8c16af65252170c047cc263b
user595 <dwightwilkins@gmail.com>
2024-01-11 18:57:52 -0600

    Parameter is now folder to generate checksums for

--- GenerateChecksum.py
+++ GenerateChecksum.py
@@ -1,10 +1,12 @@
 """
 generate a MD5 checksum of the filename specified
-i.e. /bin/python3 ~/Programming/Python/GenerateChecksum/GenerateChecksum.py GenerateChecksum.py
+i.e. /bin/python3 ~/Programming/Python/GenerateChecksum/GenerateChecksum.py .
 MD5 checksum for GenerateChecksum.py: beec6281d78aad4a3c18d97dfad80518
 """
 import hashlib
 import sys
+import glob
+import os
 
 
 def generate_checksum(pfile_path):
@@ -17,10 +19,22 @@ def generate_checksum(pfile_path):
     return md5.hexdigest()  # generate and return the hashcode
 
 
+def process_files(pfolder_path):
+    """ Use glob to get a list of all files matching the pattern 
+        in the specified folder and its sub-folders """
+    file_list = glob.glob(os.path.join(pfolder_path, '**', '*'), recursive=True)
+
+    for file_path in file_list:
+        if os.path.isfile(file_path):
+            # If the path points to a file (not a directory), generate and print the checksum
+            checksum = generate_checksum(file_path)
+            print(f"MD5 checksum for {file_path}: {checksum}")
+
+
 if len(sys.argv) != 2:
     print("Usage: python checksum_generator.py <file_path> \
          to generate a MD5 checksum")
     sys.exit(1)
 
-file_path = sys.argv[1]
-print(f"MD5 checksum for {file_path}: {generate_checksum(file_path)}")
+folder_path = sys.argv[1]
+process_files(folder_path)
